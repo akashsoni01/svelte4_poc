b6ce0892243b37c85cf28211e7edae70040d66c7
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 03:08:46 +0530

    Await block await - promise then number

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,21 +1,23 @@
 <script>
-  import Thing from "./Thing.svelte";
+  import { getRandomNumber } from "./utils.js";
 
-  let things = [
-    { id: 1, name: "apple" },
-    { id: 2, name: "banana" },
-    { id: 3, name: "carrot" },
-    { id: 4, name: "doughnut" },
-    { id: 5, name: "egg" },
-  ];
+  let promise = getRandomNumber();
 
   function handleClick() {
-    things = things.slice(1);
+    promise = getRandomNumber();
   }
 </script>
 
-<button on:click={handleClick}> Remove first thing </button>
+<button on:click={handleClick}> generate random number </button>
 
-{#each things as thing (thing.id)}
-  <Thing name={thing.name} />
-{/each}
+<!-- {#await promise}
+  <p>...waiting</p>
+{:then number}
+  <p>The number is {number}</p>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await} -->
+
+{#await promise then number}
+  <p>The number is {number}</p>
+{/await}
