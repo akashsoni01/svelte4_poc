c73d6a115c9c85f2303123c1291c5b3472c9237d
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 03:27:27 +0530

    event forwarding

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-  import Inner from "./Inner.svelte";
+  import Outer from "./Outer.svelte";
 
   function handleMessage(event) {
     alert(event.detail.text);
   }
 </script>
 
-<Inner on:message={handleMessage} />
+<Outer on:message={handleMessage} />
---
+++ my-svelte-project/src/Outer.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Inner from "./Inner.svelte";
+</script>
+
+<Inner />
+<Inner on:message />
