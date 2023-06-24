53ad1750133d6f9665daab17b01e9f755db6e769
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 02:28:29 +0530

    reactive statement

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,15 +1,16 @@
 <script>
   let count = 0;
-  $: doubled = count * 2;
 
-  function increment() {
+  function handleClick() {
     count += 1;
+  }
+  $: if (count >= 10) {
+    alert("count is dangerously high!");
+    count = 0;
   }
 </script>
 
-<button on:click={increment}>
+<button on:click={handleClick}>
   Clicked {count}
   {count === 1 ? "time" : "times"}
 </button>
-
-<p>{count} doubled is {doubled}</p>
