85779794d976d21be203aff5424db29ce4e1c494
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 02:56:30 +0530

    each colors as color

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,20 +1,53 @@
 <script>
-  let count = 0;
-
-  function increment() {
-    count += 1;
-  }
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ];
+  let selected = colors[0];
 </script>
 
-<button on:click={increment}>
-  Clicked {count}
-  {count === 1 ? "time" : "times"}
-</button>
+<h1 style="color: {selected}">Pick a colour</h1>
 
-{#if count > 10}
-  <p>{count} is greater than 10</p>
-{:else if count < 5}
-  <p>{count} is less than 5</p>
-{:else}
-  <p>{count} is between 5 and 10</p>
-{/if}
+<div>
+  {#each colors as color}
+    <button
+      aria-current={selected === color}
+      aria-label={color}
+      style="background: {color}"
+      on:click={() => (selected = color)}
+    />
+  {/each}
+</div>
+
+<style>
+  h1 {
+    transition: color 0.2s;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-gap: 5px;
+    max-width: 400px;
+  }
+
+  button {
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background: var(--color, #fff);
+    transform: translate(-2px, -2px);
+    filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2));
+    transition: all 0.1s;
+  }
+
+  button[aria-current="true"] {
+    transform: none;
+    filter: none;
+    box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
+  }
+</style>
