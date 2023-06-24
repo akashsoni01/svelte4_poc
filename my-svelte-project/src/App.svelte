79dea591f77fc5618084a2b82e5592d9c9cf8fae
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 02:52:03 +0530

    else if

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -11,6 +11,10 @@
   {count === 1 ? "time" : "times"}
 </button>
 
-{#if count > 5}
+{#if count > 10}
   <p>{count} is greater than 10</p>
+{:else if count < 5}
+  <p>{count} is less than 5</p>
+{:else}
+  <p>{count} is between 5 and 10</p>
 {/if}
