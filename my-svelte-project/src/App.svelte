d865dbd74b5547a1eb2babb1e3e7f312c8f9de9c
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 15:35:57 +0530

    transition - adding parameters (fly y and duration)

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { fade } from "svelte/transition";
+  import { fly } from "svelte/transition";
 
   let visible = true;
 </script>
@@ -10,5 +10,5 @@
 </label>
 
 {#if visible}
-  <p transition:fade>Fades in and out</p>
+  <p transition:fly={{ y: 200, duration: 2000 }}>Fades in and out</p>
 {/if}
