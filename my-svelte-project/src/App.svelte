d2a4581473c9369307b26d47623d03f8326ecbd7
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 16:06:10 +0530

    deferred transitions

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,26 +1,57 @@
 <script>
-  import { onMount } from "svelte";
-  import { typewriter } from "./transition.js";
-  import { messages } from "./loading-messages.js";
+  import { createTodoStore } from "./todos.js";
+  import TodoList from "./TodoList.svelte";
 
-  let i = -1;
-
-  onMount(() => {
-    const interval = setInterval(() => {
-      i += 1;
-      i %= messages.length;
-    }, 2500);
-
-    return () => {
-      clearInterval(interval);
-    };
-  });
+  const todos = createTodoStore([
+    { done: false, description: "write some docs" },
+    { done: false, description: "start writing blog post" },
+    { done: true, description: "buy some milk" },
+    { done: false, description: "mow the lawn" },
+    { done: false, description: "feed the turtle" },
+    { done: false, description: "fix some bugs" },
+  ]);
 </script>
 
-<h1>loading...</h1>
+<div class="board">
+  <input
+    placeholder="what needs to be done?"
+    on:keydown={(e) => {
+      if (e.key === "Enter") {
+        todos.add(e.currentTarget.value);
+        e.currentTarget.value = "";
+      }
+    }}
+  />
 
-{#key i}
-  <p in:typewriter={{ speed: 1 }}>
-    {messages[i] || ""}
-  </p>
-{/key}
+  <div class="todo">
+    <h2>todo</h2>
+    <TodoList store={todos} done={false} />
+  </div>
+
+  <div class="done">
+    <h2>done</h2>
+    <TodoList store={todos} done={true} />
+  </div>
+</div>
+
+<style>
+  .board {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1em;
+    max-width: 36em;
+    margin: 0 auto;
+  }
+
+  .board > input {
+    font-size: 1.4em;
+    grid-column: 1/3;
+    padding: 0.5em;
+    margin: 0 0 1rem 0;
+  }
+
+  h2 {
+    font-size: 2em;
+    font-weight: 200;
+  }
+</style>
--- my-svelte-project/src/transition.js
+++ my-svelte-project/src/transition.js
@@ -1,18 +1,20 @@
-export function typewriter(node, { speed = 1 }) {
-    const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE;
+import { crossfade } from 'svelte/transition';
+import { quintOut } from 'svelte/easing';
 
-    if (!valid) {
-        throw new Error(`This transition only works on elements with a single text node child`);
+export const [send, receive] = crossfade({
+    duration: (d) => Math.sqrt(d * 200),
+
+    fallback(node, params) {
+        const style = getComputedStyle(node);
+        const transform = style.transform === 'none' ? '' : style.transform;
+
+        return {
+            duration: 600,
+            easing: quintOut,
+            css: (t) => `
+				transform: ${transform} scale(${t});
+				opacity: ${t}
+			`
+        };
     }
-
-    const text = node.textContent;
-    const duration = text.length / (speed * 0.01);
-
-    return {
-        duration,
-        tick: (t) => {
-            const i = Math.trunc(text.length * t);
-            node.textContent = text.slice(0, i);
-        }
-    };
-}
+});
