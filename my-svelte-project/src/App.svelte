5be6e9fe38c3a5dfb94fd1339796e1d0ef848457
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 13:56:28 +0530

    bind text area

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,40 +1,27 @@
 <script>
-  let scoops = 1;
-  let flavours = [];
-
-  const formatter = new Intl.ListFormat("en", {
-    style: "long",
-    type: "conjunction",
-  });
+  // import { marked } from "marked";
+  let value = `Some words are *italic*, some are **bold**\n\n- lists\n- are\n- cool`;
 </script>
 
-<h2>Size</h2>
+<div class="grid">
+  input
+  <textarea bind:value />
 
-{#each [1, 2, 3] as number}
-  <label>
-    <input type="radio" name="scoops" value={number} bind:group={scoops} />
+  output
+  <div>{@html value}</div>
+</div>
 
-    {number}
-    {number === 1 ? "scoop" : "scoops"}
-  </label>
-{/each}
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: 5em 1fr;
+    grid-template-rows: 1fr 1fr;
+    grid-gap: 1em;
+    height: 100%;
+  }
 
-<h2>Flavours</h2>
-
-<select multiple bind:value={flavours}>
-  {#each ["cookies and cream", "mint choc chip", "raspberry ripple"] as flavour}
-    <option>{flavour}</option>
-  {/each}
-</select>
-
-{#if flavours.length === 0}
-  <p>Please select at least one flavour</p>
-{:else if flavours.length > scoops}
-  <p>Can't order more flavours than scoops!</p>
-{:else}
-  <p>
-    You ordered {scoops}
-    {scoops === 1 ? "scoop" : "scoops"}
-    of {formatter.format(flavours)}
-  </p>
-{/if}
+  textarea {
+    flex: 1;
+    resize: none;
+  }
+</style>
