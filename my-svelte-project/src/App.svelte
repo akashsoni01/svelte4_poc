a38fa37a36f33d51a457049c0cbba26da29fac4e
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 19:25:07 +0530

    actions - use: directive

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,57 +1,138 @@
 <script>
-  import { createTodoStore } from "./todos.js";
-  import TodoList from "./TodoList.svelte";
+  import Canvas from "./Canvas.svelte";
+  import { trapFocus } from "./actions.js";
 
-  const todos = createTodoStore([
-    { done: false, description: "write some docs" },
-    { done: false, description: "start writing blog post" },
-    { done: true, description: "buy some milk" },
-    { done: false, description: "mow the lawn" },
-    { done: false, description: "feed the turtle" },
-    { done: false, description: "fix some bugs" },
-  ]);
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+    "white",
+    "black",
+  ];
+  let selected = colors[0];
+  let size = 10;
+
+  let showMenu = true;
 </script>
 
-<div class="board">
-  <input
-    placeholder="what needs to be done?"
-    on:keydown={(e) => {
-      if (e.key === "Enter") {
-        todos.add(e.currentTarget.value);
-        e.currentTarget.value = "";
-      }
-    }}
-  />
+<div class="container">
+  <Canvas color={selected} {size} />
 
-  <div class="todo">
-    <h2>todo</h2>
-    <TodoList store={todos} done={false} />
-  </div>
+  {#if showMenu}
+    <div
+      class="modal-background"
+      on:click|self={() => (showMenu = false)}
+      on:keydown={(e) => {
+        if (e.key === "Escape") showMenu = false;
+      }}
+    >
+      <div class="menu">
+        <div class="colors">
+          {#each colors as color}
+            <button
+              class="color"
+              aria-label={color}
+              aria-current={selected === color}
+              style="--color: {color}"
+              on:click={() => {
+                selected = color;
+              }}
+            />
+          {/each}
+        </div>
 
-  <div class="done">
-    <h2>done</h2>
-    <TodoList store={todos} done={true} />
+        <label>
+          small
+          <input type="range" bind:value={size} min="1" max="50" />
+          large
+        </label>
+      </div>
+    </div>
+  {/if}
+
+  <div class="controls">
+    <button class="show-menu" on:click={() => (showMenu = !showMenu)}>
+      {showMenu ? "close" : "menu"}
+    </button>
   </div>
 </div>
 
 <style>
-  .board {
+  .container {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .controls {
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: 1em;
+  }
+
+  .show-menu {
+    width: 5em;
+  }
+
+  .modal-background {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(20px);
+  }
+
+  .menu {
+    position: relative;
+    background: var(--bg-2);
+    width: calc(100% - 2em);
+    max-width: 28em;
+    padding: 1em 1em 0.5em 1em;
+    border-radius: 1em;
+    box-sizing: border-box;
+    user-select: none;
+  }
+
+  .colors {
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 1em;
-    max-width: 36em;
-    margin: 0 auto;
+    align-items: center;
+    grid-template-columns: repeat(9, 1fr);
+    grid-gap: 0.5em;
   }
 
-  .board > input {
-    font-size: 1.4em;
-    grid-column: 1/3;
-    padding: 0.5em;
-    margin: 0 0 1rem 0;
+  .color {
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background: var(--color, #fff);
+    transform: none;
+    filter: drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.2));
+    transition: all 0.1s;
   }
 
-  h2 {
-    font-size: 2em;
-    font-weight: 200;
+  .color[aria-current="true"] {
+    transform: translate(1px, 1px);
+    filter: none;
+    box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
+  }
+
+  .menu label {
+    display: flex;
+    width: 100%;
+    margin: 1em 0 0 0;
+  }
+
+  .menu input {
+    flex: 1;
   }
 </style>
