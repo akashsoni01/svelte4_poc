7cee8306cf823fa395f92e12f8fed8351961d175
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 13:43:30 +0530

    select option binding

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,16 +1,46 @@
 <script>
-  let yes = false;
+  let questions = [
+    {
+      id: 1,
+      text: `Where did you go to school?`,
+    },
+    {
+      id: 2,
+      text: `What is your mother's name?`,
+    },
+    {
+      id: 3,
+      text: `What is another personal fact that an attacker could easily find with Google?`,
+    },
+  ];
+
+  let selected;
+
+  let answer = "";
+
+  function handleSubmit() {
+    alert(
+      `answered question ${selected.id} (${selected.text}) with "${answer}"`
+    );
+  }
 </script>
 
-<label>
-  <input type="checkbox" bind:checked={yes} />
-  Yes! Send me regular email spam
-</label>
+<h2>Insecurity questions</h2>
 
-{#if yes}
-  <p>Thank you. We will bombard your inbox and sell your personal details.</p>
-{:else}
-  <p>You must opt in to continue. If you're not paying, you're the product.</p>
-{/if}
+<form on:submit|preventDefault={handleSubmit}>
+  <select bind:value={selected} on:change={() => (answer = "")}>
+    {#each questions as question}
+      <option value={question}>
+        {question.text}
+      </option>
+    {/each}
+  </select>
 
-<button disabled={!yes}>Subscribe</button>
+  <input bind:value={answer} />
+
+  <button disabled={!answer} type="submit"> Submit </button>
+</form>
+
+<p>
+  selected question {selected ? selected.id : "[waiting...]"}
+</p>
