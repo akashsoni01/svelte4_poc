581fc061ee6162be43a75e242b6297f93fdfa961
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 13:15:12 +0530

    binding of input type range, number

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,7 +1,16 @@
 <script>
-  let name = "world";
+  let a = 1;
+  let b = 2;
 </script>
 
-<input bind:value={name} />
+<label>
+  <input type="number" bind:value={a} min="0" max="10" />
+  <input type="range" bind:value={a} min="0" max="10" />
+</label>
 
-<h1>Hello {name}!</h1>
+<label>
+  <input type="number" bind:value={b} min="0" max="10" />
+  <input type="range" bind:value={b} min="0" max="10" />
+</label>
+
+<p>{a} + {b} = {a + b}</p>
