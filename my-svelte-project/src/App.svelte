b71b412f2ee16bb4e91295a74331be0515b3adc2
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 13:17:08 +0530

    binding checkbox

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,16 +1,16 @@
 <script>
-  let a = 1;
-  let b = 2;
+  let yes = false;
 </script>
 
 <label>
-  <input type="number" bind:value={a} min="0" max="10" />
-  <input type="range" bind:value={a} min="0" max="10" />
+  <input type="checkbox" bind:checked={yes} />
+  Yes! Send me regular email spam
 </label>
 
-<label>
-  <input type="number" bind:value={b} min="0" max="10" />
-  <input type="range" bind:value={b} min="0" max="10" />
-</label>
+{#if yes}
+  <p>Thank you. We will bombard your inbox and sell your personal details.</p>
+{:else}
+  <p>You must opt in to continue. If you're not paying, you're the product.</p>
+{/if}
 
-<p>{a} + {b} = {a + b}</p>
+<button disabled={!yes}>Subscribe</button>
