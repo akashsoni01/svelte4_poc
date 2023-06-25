b4df41e661bcbdc14e26030b5365860df47e9ba9
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 15:33:40 +0530

    transition - fade

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,72 +1,14 @@
 <script>
-  import { spring } from "svelte/motion";
+  import { fade } from "svelte/transition";
 
-  let coords = spring(
-    { x: 50, y: 50 },
-    {
-      stiffness: 0.1,
-      damping: 0.25,
-    }
-  );
-  let size = spring(10);
+  let visible = true;
 </script>
 
-<svg
-  on:mousemove={(e) => {
-    coords.set({ x: e.clientX, y: e.clientY });
-  }}
-  on:mousedown={() => size.set(30)}
-  on:mouseup={() => size.set(10)}
->
-  <circle cx={$coords.x} cy={$coords.y} r={$size} />
-</svg>
+<label>
+  <input type="checkbox" bind:checked={visible} />
+  visible
+</label>
 
-<div class="controls">
-  <label>
-    <h3>stiffness ({coords.stiffness})</h3>
-    <input
-      bind:value={coords.stiffness}
-      type="range"
-      min="0.01"
-      max="1"
-      step="0.01"
-    />
-  </label>
-
-  <label>
-    <h3>damping ({coords.damping})</h3>
-    <input
-      bind:value={coords.damping}
-      type="range"
-      min="0.01"
-      max="1"
-      step="0.01"
-    />
-  </label>
-</div>
-
-<style>
-  svg {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-  }
-
-  circle {
-    fill: #ff3e00;
-  }
-
-  .controls {
-    position: absolute;
-    top: 1em;
-    right: 1em;
-    width: 200px;
-    user-select: none;
-  }
-
-  .controls input {
-    width: 100%;
-  }
-</style>
+{#if visible}
+  <p transition:fade>Fades in and out</p>
+{/if}
