9779af9a09cdded2320c7abe5555dcfecfb6b96c
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 15:31:28 +0530

    motion - tweened

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,72 +1,28 @@
 <script>
-  import { spring } from "svelte/motion";
+  import { tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
 
-  let coords = spring(
-    { x: 50, y: 50 },
-    {
-      stiffness: 0.1,
-      damping: 0.25,
-    }
-  );
-  let size = spring(10);
+  const progress = tweened(0, {
+    duration: 400,
+    easing: cubicOut,
+  });
 </script>
 
-<svg
-  on:mousemove={(e) => {
-    coords.set({ x: e.clientX, y: e.clientY });
-  }}
-  on:mousedown={() => size.set(30)}
-  on:mouseup={() => size.set(10)}
->
-  <circle cx={$coords.x} cy={$coords.y} r={$size} />
-</svg>
+<progress value={$progress} />
 
-<div class="controls">
-  <label>
-    <h3>stiffness ({coords.stiffness})</h3>
-    <input
-      bind:value={coords.stiffness}
-      type="range"
-      min="0.01"
-      max="1"
-      step="0.01"
-    />
-  </label>
+<button on:click={() => progress.set(0)}> 0% </button>
 
-  <label>
-    <h3>damping ({coords.damping})</h3>
-    <input
-      bind:value={coords.damping}
-      type="range"
-      min="0.01"
-      max="1"
-      step="0.01"
-    />
-  </label>
-</div>
+<button on:click={() => progress.set(0.25)}> 25% </button>
+
+<button on:click={() => progress.set(0.5)}> 50% </button>
+
+<button on:click={() => progress.set(0.75)}> 75% </button>
+
+<button on:click={() => progress.set(1)}> 100% </button>
 
 <style>
-  svg {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-  }
-
-  circle {
-    fill: #ff3e00;
-  }
-
-  .controls {
-    position: absolute;
-    top: 1em;
-    right: 1em;
-    width: 200px;
-    user-select: none;
-  }
-
-  .controls input {
+  progress {
+    display: block;
     width: 100%;
   }
 </style>
