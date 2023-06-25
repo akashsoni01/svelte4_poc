98b90296f84c03bb771682e871a1822e9afffc2f
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 14:38:37 +0530

    store - writable

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,35 +1,18 @@
 <script>
-  import { tick } from "svelte";
+  import { count } from "./stores.js";
+  import Incrementer from "./Incrementer.svelte";
+  import Decrementer from "./Decrementer.svelte";
+  import Resetter from "./Resetter.svelte";
 
-  let text = `Select some text and hit the tab key to toggle uppercase`;
+  let count_value;
 
-  async function handleKeydown(event) {
-    if (event.key !== "Tab") return;
-
-    event.preventDefault();
-
-    const { selectionStart, selectionEnd, value } = this;
-    const selection = value.slice(selectionStart, selectionEnd);
-
-    const replacement = /[a-z]/.test(selection)
-      ? selection.toUpperCase()
-      : selection.toLowerCase();
-
-    text =
-      value.slice(0, selectionStart) + replacement + value.slice(selectionEnd);
-
-    await tick();
-    this.selectionStart = selectionStart;
-    this.selectionEnd = selectionEnd;
-  }
+  count.subscribe((value) => {
+    count_value = value;
+  });
 </script>
 
-<textarea value={text} on:keydown={handleKeydown} />
+<h1>The count is {count_value}</h1>
 
-<style>
-  textarea {
-    width: 100%;
-    height: 100%;
-    resize: none;
-  }
-</style>
+<Incrementer />
+<Decrementer />
+<Resetter />
