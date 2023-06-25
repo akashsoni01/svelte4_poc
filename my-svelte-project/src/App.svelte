bb96bf2f8825a19b06773e1a6f5c25f05aff942d
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 15:47:40 +0530

    custom js transition ( typewriter animation - like chat gpt)

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,22 +1,25 @@
 <script>
-  import { fade } from "svelte/transition";
-  import { elasticOut } from "svelte/easing";
+  let visible = false;
 
-  let visible = true;
+  function typewriter(node, { speed = 1 }) {
+    const valid =
+      node.childNodes.length === 1 &&
+      node.childNodes[0].nodeType === Node.TEXT_NODE;
 
-  function spin(node, { duration }) {
+    if (!valid) {
+      throw new Error(
+        `This transition only works on elements with a single text node child`
+      );
+    }
+
+    const text = node.textContent;
+    const duration = text.length / (speed * 0.01);
+
     return {
       duration,
-      css: (t) => {
-        const eased = elasticOut(t);
-
-        return `
-					transform: scale(${eased}) rotate(${eased * 1080}deg);
-					color: hsl(
-						${Math.trunc(t * 360)},
-						${Math.min(100, 1000 * (1 - t))}%,
-						${Math.min(50, 500 * (1 - t))}%
-					);`;
+      tick: (t) => {
+        const i = Math.trunc(text.length * t);
+        node.textContent = text.slice(0, i);
       },
     };
   }
@@ -28,22 +31,5 @@
 </label>
 
 {#if visible}
-  <div class="centered" in:spin={{ duration: 8000 }} out:fade>
-    <span>transitions!</span>
-  </div>
+  <p transition:typewriter>The quick brown fox jumps over the lazy dog</p>
 {/if}
-
-<style>
-  .centered {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  span {
-    position: absolute;
-    transform: translate(-50%, -50%);
-    font-size: 4em;
-  }
-</style>
