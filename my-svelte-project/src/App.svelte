58ae4bdfb1a52a7a7e4c29e618637a06a0533a64
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 14:46:24 +0530

    store - trick $ - unsubscribe to resolve memory leak

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,17 +1,25 @@
 <script>
+  // import { onDestroy } from "svelte";
+
   import { count } from "./stores.js";
   import Incrementer from "./Incrementer.svelte";
   import Decrementer from "./Decrementer.svelte";
   import Resetter from "./Resetter.svelte";
 
-  let count_value;
+  // let count_value;
 
-  count.subscribe((value) => {
-    count_value = value;
-  });
+  // count.subscribe((value) => {
+  //   count_value = value;
+  // });
+
+  // const unsubscribe = count.subscribe((value) => {
+  //   count_value = value;
+  // });
+
+  // onDestroy(unsubscribe);
 </script>
 
-<h1>The count is {count_value}</h1>
+<h1>The count is {$count}</h1>
 
 <Incrementer />
 <Decrementer />
