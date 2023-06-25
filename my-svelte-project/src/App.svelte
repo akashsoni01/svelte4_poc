2dc2a485a7e609c75834c36ca80969e47a65d98a
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 19:42:32 +0530

    each block binding

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,16 +1,66 @@
 <script>
-  let html = "<p>Write some text!</p>";
+  let todos = [
+    { done: false, text: "finish Svelte tutorial" },
+    { done: false, text: "build an app" },
+    { done: false, text: "world domination" },
+  ];
+
+  function add() {
+    todos = todos.concat({
+      done: false,
+      text: "",
+    });
+  }
+
+  function clear() {
+    todos = todos.filter((t) => !t.done);
+  }
+
+  $: remaining = todos.filter((t) => !t.done).length;
 </script>
 
-<div bind:innerHTML={html} contenteditable />
-<!-- <div bind:textContent={html} contenteditable /> -->
+<div class="centered">
+  <h1>todos</h1>
 
-<pre>{html}</pre>
+  <ul class="todos">
+    {#each todos as todo}
+      <li class:done={todo.done}>
+        <input type="checkbox" bind:checked={todo.done} />
+
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          bind:value={todo.text}
+        />
+      </li>
+    {/each}
+  </ul>
+
+  <p>{remaining} remaining</p>
+
+  <button on:click={add}> Add new </button>
+
+  <button on:click={clear}> Clear completed </button>
+</div>
 
 <style>
-  [contenteditable] {
+  .centered {
+    max-width: 20em;
+    margin: 0 auto;
+  }
+
+  .done {
+    opacity: 0.4;
+  }
+
+  li {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 1;
     padding: 0.5em;
-    border: 1px solid #eee;
-    border-radius: 4px;
+    margin: -0.2em 0;
+    border: none;
   }
 </style>
