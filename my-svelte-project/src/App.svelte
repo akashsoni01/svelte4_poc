4b71784762c8d66674935e438ef08de50f75242c
Akash Soni <akashsoni8285@gmail.com>
2023-06-25 15:45:31 +0530

    custom css

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,7 +1,25 @@
 <script>
-  import { fade, fly } from "svelte/transition";
+  import { fade } from "svelte/transition";
+  import { elasticOut } from "svelte/easing";
 
   let visible = true;
+
+  function spin(node, { duration }) {
+    return {
+      duration,
+      css: (t) => {
+        const eased = elasticOut(t);
+
+        return `
+					transform: scale(${eased}) rotate(${eased * 1080}deg);
+					color: hsl(
+						${Math.trunc(t * 360)},
+						${Math.min(100, 1000 * (1 - t))}%,
+						${Math.min(50, 500 * (1 - t))}%
+					);`;
+      },
+    };
+  }
 </script>
 
 <label>
@@ -10,5 +28,22 @@
 </label>
 
 {#if visible}
-  <p in:fly={{ y: 200, duration: 2000 }} out:fade>Flies in, fades out</p>
+  <div class="centered" in:spin={{ duration: 8000 }} out:fade>
+    <span>transitions!</span>
+  </div>
 {/if}
+
+<style>
+  .centered {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  span {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    font-size: 4em;
+  }
+</style>
