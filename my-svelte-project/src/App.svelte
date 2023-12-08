6a2d72a69679b7029178f4427c69712b2296ab6d
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2023-12-08 21:54:00 +0530

    animation removed

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,57 +1,18 @@
 <script>
   import { Router, Route, Link } from "svelte-routing";
-  import { fade } from "svelte/transition";
   import Home from "./routes/Home.svelte";
   import About from "./routes/About.svelte";
   import NotFound from "./routes/NotFound.svelte";
-  import { writable } from "svelte/store";
-
-  // This store holds the state of whether the transition is in progress
-  const isTransitioning = writable(false);
-
-  // Function to call when starting a transition
-  function startTransition() {
-    isTransitioning.set(true);
-  }
-
-  // Function to call when ended a transition
-  function endTransition() {
-    isTransitioning.set(false);
-  }
 </script>
 
 <Router>
   <nav>
-    <Link to="/" on:click={startTransition}>Home</Link>
-    <Link to="/about" on:click={startTransition}>About</Link>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
   </nav>
   <section>
-    <Route
-      path="/"
-      component={Home}
-      transition={fade}
-      transitionState={isTransitioning}
-      on:introend={endTransition}
-      on:outrostart={startTransition}
-      on:outroend={endTransition}
-    />
-    <Route
-      path="/about"
-      component={About}
-      transition={fade}
-      transitionState={isTransitioning}
-      on:introend={endTransition}
-      on:outrostart={startTransition}
-      on:outroend={endTransition}
-    />
-    <Route
-      path="*"
-      component={NotFound}
-      transition={fade}
-      transitionState={isTransitioning}
-      on:introend={endTransition}
-      on:outrostart={startTransition}
-      on:outroend={endTransition}
-    />
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="*" component={NotFound} />
   </section>
 </Router>
